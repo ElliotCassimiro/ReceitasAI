203ad1f63828635f2c84f572b47cb05efb5c9054
Elliotico <48328072+Elliotico@users.noreply.github.com>
2023-03-28 01:06:15 -0300

    Mensagem inicial Ok

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ function init() {
   res_msg.setAttribute("class","left");
 
   document.getElementById('mensagem').appendChild(res_msg);
-
 }
 
   document.getElementById('resposta').addEventListener("click", async (e) => {
@@ -22,8 +21,8 @@ function init() {
             res = JSON.stringify(data.data.response)
           } )
           
-          let msg_req = document.createElement('div');
-          let msg_res = document.createElement('div');
+          let data_req = document.createElement('div');
+          let data_res = document.createElement('div');
 
           let con1 = document.createElement('div');
           let con2 = document.createElement('div');
@@ -31,24 +30,31 @@ function init() {
           con1.setAttribute("class","msgCon1");
           con2.setAttribute("class","msgCon2");
 
-          msg_req.innerHTML = req ;
-          msg_res.innerHTML = res ;
+          data_req.innerHTML = req ;
+          data_res.innerHTML = res ;
 
-          msg_req.setAttribute("class","right");
-          msg_res.setAttribute("class","left");
+          data_req.setAttribute("class","right");
+          data_res.setAttribute("class","left");
 
           let message = document.getElementById('mensagem');
 
           message.appendChild(Con1);
           message.appendChild(Con2);
 
-          Con1.appendChild(msg_req);
-          Con2.appendChild(msg_res);
+          Con1.appendChild(data_req);
+          Con2.appendChild(data_res);
 
           document.getElementById('msg_enviada').value ="";
-
-
+      
+          function scroll() {
+            var scrollMsg = document.getElementById('mensagem')
+            scrollMsg.scrollTop = scrollMsg.scrollHeight ;
+        }
+        scroll();
+     
       }
+
+
 
 
   });
