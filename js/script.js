71f02ac2622b6b000552e9b33f2def387921cfa4
Elliotico <48328072+Elliotico@users.noreply.github.com>
2023-03-28 08:03:24 -0300

    Issues

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 function init() {
   let res_msg = document.createElement('div')
-  res_msg.innerHTML = "Olá sou o BotCooker, o que vamos cozinhar?" ;
+  res_msg.innerHTML = "Hello, I am BotCooker! Type in ingredient to get a recipe..." ;
   res_msg.setAttribute("class","left");
 
   document.getElementById('mensagem').appendChild(res_msg);
@@ -10,14 +10,13 @@ function init() {
   document.getElementById('resposta').addEventListener("click", async (e) => {
   e.preventDefault();
 
-      var req = document.getElementById('enviar_msg').value ;
+      var req = document.getElementById('msg_enviada').value;
       if (req == undefined || req == "") {
-
       }
       else{
           var res = "";
           
-          await axios.get(`https://api.api-ninjas.com/v1/recipe?query=`).then(data => {
+          await axios.get(`https://api.api-ninjas.com/v1/recipe?query=${req}`).then(data => {
             res = JSON.stringify(data.data.response)
           } )
           
@@ -44,17 +43,6 @@ function init() {
           Con1.appendChild(data_req);
           Con2.appendChild(data_res);
 
-          document.getElementById('msg_enviada').value ="";
-      
-          function scroll() {
-            var scrollMsg = document.getElementById('mensagem')
-            scrollMsg.scrollTop = scrollMsg.scrollHeight ;
-        }
-        scroll();
-     
+          document.getElementById('msg_enviada').value ="";    
       }
-
-
-
-
-  });
+  }) ;
